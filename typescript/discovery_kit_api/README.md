e0bafb3ac8887d71326ef34888499cdcf7b98d6f
Ben Blackmore <ben.blackmore@steadybit.com>
2022-08-16 16:36:13 +0200

    refactor: auto-generate TypeScript types

---
+++ typescript/discovery_kit_api/README.md
@@ -0,0 +1,16 @@
+# @steadybit/discovery-kit-api
+
+This Node.js package contains TypeScript type definitions that you might
+find helpful when authoring Steadybit discoveries with TypeScript.
+
+## Installation
+
+```sh
+npm install --save-dev @steadybit/discovery-kit-api
+```
+
+## Usage
+
+```typescript
+import { DiscoveryList } from '@steadybit/@steadybit/discovery-kit-api';
+```
---
+++ typescript/discovery_kit_api/index.test.ts
@@ -0,0 +1,100 @@
+// SPDX-License-Identifier: MIT
+// SPDX-FileCopyrightText: 2022 Steadybit GmbH
+
+import {
+	AttributeDescriptions, DiscoveredTargets,
+	DiscoveryDescription,
+	DiscoveryKitError,
+	DiscoveryList, TargetDescription
+
+} from "./index";
+
+export const discoveryList: DiscoveryList = {
+	discoveries: [
+		{
+			method: "GET",
+			path: "/action"
+		}
+	],
+
+	targetTypes: [
+		{
+			method: "GET",
+			path: "/action"
+		}
+	],
+
+	targetAttributes: [
+		{
+			method: "GET",
+			path: "/action"
+		}
+	]
+};
+
+export const discoveryDescription: DiscoveryDescription = {
+	discover: {
+		method: "GET",
+		path: "/",
+		callInterval: "5m"
+	},
+	id: "42",
+	restrictTo: "LEADER"
+};
+
+export const attributeDescriptions: AttributeDescriptions = {
+	attributes: [
+		{
+			attribute: "k8s.deployment",
+			label: {
+				one: "Kubernetes deployment",
+				other: "Kubernetes deployments"
+			}
+		}
+	]
+};
+
+export const discoveredTargets: DiscoveredTargets = {
+	targets: [
+		{
+			attributes: {
+				foo: ["bar"]
+			},
+			id: "i",
+			label: "l",
+			targetType: "t"
+		}
+	]
+};
+
+export const targetDescription: TargetDescription = {
+	category: 'basic',
+	icon: 'data:...',
+	id: 'id',
+	version: '1.0.0',
+	label: {
+		one: 'one',
+		other: 'other'
+	},
+	table: {
+		columns: [
+			{
+				attribute: 'attr',
+				fallbackAttributes: ['a', 'b']
+			}
+		],
+		orderBy: [
+			{
+				attribute: 'attr',
+				direction: 'DESC'
+			}
+		]
+	}
+};
+
+export const discoveryKitError: DiscoveryKitError = {
+	detail: "d",
+	instance: "i",
+	title: "t",
+	type: "t"
+};
