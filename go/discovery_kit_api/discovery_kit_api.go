1e3da3b802abd0bcc1e1fe734464730bc855ba9f
Daniel Reuter <daniel@steadybit.com>
2023-03-29 15:19:24 +0200

    feat: removed restriction of discoveries to AWS agents.

--- go/discovery_kit_api/discovery_kit_api.go
+++ go/discovery_kit_api/discovery_kit_api.go
@@ -27,7 +27,6 @@ const (
 // Defines values for DiscoveryDescriptionRestrictTo.
 const (
 	ANY    DiscoveryDescriptionRestrictTo = "ANY"
-	AWS    DiscoveryDescriptionRestrictTo = "AWS"
 	LEADER DiscoveryDescriptionRestrictTo = "LEADER"
 )
 
@@ -107,11 +106,11 @@ type DiscoveryDescription struct {
 	// A technical ID that is used to uniquely identify this type of discovery. You will typically want to use something like `org.example.discoveries.my-fancy-discovery`.
 	Id string `json:"id"`
 
-	// If the agent is deployed as a daemonset in Kubernetes, should the discovery only be called from the leader agent? This can be helpful to avoid duplicate targets for every running agent. You may alternatively define that the discovery should run only for Steadybit agents operating within the AWS agent mode (as defined by the `STEADYBIT_AGENT_MODE` Steadybit agent environment variable / the `agent.mode` Steadybit agent Helm chart value).
+	// If the agent is deployed as a daemonset in Kubernetes, should the discovery only be called from the leader agent? This can be helpful to avoid duplicate targets for every running agent.
 	RestrictTo *DiscoveryDescriptionRestrictTo `json:"restrictTo,omitempty"`
 }
 
-// If the agent is deployed as a daemonset in Kubernetes, should the discovery only be called from the leader agent? This can be helpful to avoid duplicate targets for every running agent. You may alternatively define that the discovery should run only for Steadybit agents operating within the AWS agent mode (as defined by the `STEADYBIT_AGENT_MODE` Steadybit agent environment variable / the `agent.mode` Steadybit agent Helm chart value).
+// If the agent is deployed as a daemonset in Kubernetes, should the discovery only be called from the leader agent? This can be helpful to avoid duplicate targets for every running agent.
 type DiscoveryDescriptionRestrictTo string
 
 // RFC 7807 Problem Details for HTTP APIs compliant response body for error scenarios
